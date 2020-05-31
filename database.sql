4c903e77561125d1bef289bf5956714673486dac
kgauzampe <kgauza.mpe@gmail.com>
2020-05-31 15:59:50 +0200

    fixed

--- database.sql
+++ database.sql
@@ -1,84 +1,83 @@
+-- create Umuzi database
 CREATE DATABASE Umuzi;
 
-CREATE TABLE customers (
-    customerid serial PRIMARY KEY,
+-- create customers table
+CREATE TABLE Customers (
+    customerid serial primary key,
     firstname varchar(50),
     lastname varchar(50),
     gender varchar,
-    address varchar(200), 
+    address varchar(200),
     phone int,
     email varchar(100),
     city varchar(20),
-    country varchar(50),
-  
+    country varchar(50)
 );
 
-INSERT INTO customers ("customerid", "firstname", "lastname", "gender", "address", "phone", "email", "city", "country") VALUES
+-- insert records into table
+INSERT INTO Customers ("customerid", "firstname", "lastname", "gender", "address", "phone", "email", "city", "country") VALUES
 (1,	'John',	'Hibert',	'Male',	'284 chaucer st',	84789657,	'john@gmail.com',	'Johannesburg',	'South Africa'),
 (2,	'Thando',	'Sithole',	'Female',	'240 Sect 1',	794445584,	'thando@gmail.com',	'Cape Town',	'South Africa'),
 (3,	'Leon',	'Glen',	'Male',	'81 Everton Rd,Gilits',	820832830,	'Leon@gmail.com',	'Durban',	'South Africa'),
 (4,	'Charl',	'Muller',	'Male',	'290A Dorset Ecke',	856872553,	'Charl.muller@yahoo.com',	'Berlin',	'Germany'),
 (5,	'Julia',	'Stein',	'Female',	'2 Wernerring',	672445058,	'Js234@yahoo.com',	'Frankfurt',	'Germany');
 
-
-
-CREATE TABLE employees (
-    employeeid serial PRIMARY KEY,
+-- create Employees table
+CREATE TABLE Employees (
+    employeeid serial primary key,
     firstname varchar(50),
     lastname varchar(50),
     email varchar(100),
-    jobtitle varchar(20),
-   
+    jobtitle varchar(20)
 );
 
-INSERT INTO employees ("employeeid", "firstname", "lastname", "email", "jobtitle") VALUES
+-- insert records into table
+INSERT INTO Employees ("employeeid", "firstname", "lastname", "email", "jobtitle") VALUES
 (1,	'Kani',	'Matthew',	'mat@gmail.com',	'Manager'),
 (2,	'Lesly',	'Cronje',	'LesC@gmail.com',	'Clerk'),
 (3,	'Gideon',	'Maduku',	'm@gmail.com',	'Accountant');
 
+-- create Products table
+CREATE TABLE Products (
+    productid serial primary key,
+    productname varchar(100),
+    description varchar(300),
+    buyprice decimal
+);
 
-CREATE TABLE orders (
-    orderid serail PRIMARY KEY,
+-- insert records into table
+INSERT INTO Products ("productid", "productname", "description", "buyprice") VALUES
+(1,	'Harley Davidson Chopper',	'This replica features working kickstands, font suspension, gear-shift lever', 150.75),
+(2,	'Classic Car', 'Turnable front wheels , steering function', 550.75),
+(3,	'Sports Car', 'Turnable front wheels, steering function',	700.60);
+
+-- create Orders table
+CREATE TABLE Orders (
+    orderid serial primary key,
     productid int REFERENCES products(productid),
     paymentid int REFERENCES payments(paymentid),
     fulfilledbyemployeeid int,
     daterequired date,
     dateshipped date,
-    status varchar(20),
-   
- 
+    status varchar(20)
 );
 
-INSERT INTO orders ("orderid", "productid", "paymentid", "fulfilledbyemployeeid", "daterequired", "dateshipped", "status") VALUES
+-- insert records into table
+INSERT INTO Orders ("orderid", "productid", "paymentid", "fulfilledbyemployeeid", "daterequired", "dateshipped", "status") VALUES
 (1,	1,	1,	2,	'2018-05-09',	NULL,	'Not shipped'),
 (2,	1,	2,	2,	'2018-04-09',	'2018-03-09',	'Shipped'),
 (3,	3,	3,	3,	'2018-06-09',	NULL,	'Not shipped');
 
-
-CREATE TABLE payments (
+-- create Payments table
+CREATE TABLE Payments (
     customerid int,
-    paymentid serial PRIMARY KEY,
+    paymentid serial primary key,
     paymentdate date,
-    amount numeric,
-    
+    amount decimal
 );
 
-INSERT INTO payments ("customerid", "paymentid", "paymentdate", "amount") VALUES
-(1,	1,	'2018-01-09 00:00:00',	150.75),
-(5,	2,	'2018-03-09 00:00:00',	150.75),
-(4,	3,	'2018-03-09 00:00:00',	700.60);
-
-
-
-CREATE TABLE products (
-    productid serial PRIMARY KEY,
-    productname varchar(100),
-    description varchar(300),
-    buyprice numeric,
-   
-);
-
-INSERT INTO products ("productid", "productname", "description", "buyprice") VALUES
-(1,	'Harley Davidson Chopper',	'This replica features working kickstands, font suspension, gear-shift lever',	150.75),
-(2,	'Classic Car',	'Turnable front wheels , steering function',	550.75),
-(3,	'Sports Car',	'Turnable front wheels, steering function',	700.60);
+-- insert records into table
+INSERT INTO Payments ("customerid", "paymentid", "paymentdate", "amount") VALUES
+(1,	1,	'2018-01-09',	150.75),
+(5,	2,	'2018-03-09',	150.75),
+(4,	3,	'2018-03-09',	700.60);
--- querying.sql
+++ querying.sql
@@ -1,17 +1,33 @@
-select * from customers
-select firstname from customers;
-select firstname from customers where customerid = 1;
-update customers set firstname = 'Lerato Mabitso' where customerid = 1;
-delete from customers where customerid = 2;
-select count(status) from orders; 
-select max(amount) from payments;
-select * from customers order by country;
-select productname from products where buyprice between 100 and 600;
-select * from customers where country = 'Germany' and city = 'Berlin';
-select * from customers where city = 'Durban' or city = 'Cape Town';
-select * from products where buyprice > 500;
-select sum(amount) from payments;
-select count(status) from orders where status = 'Shipped';
-select avg(buyprice) from products;
-select * from customers inner join payments on payments.customerid = customers.customerid;
-select * from products where description = 'turnable front wheels, steering function';
+SELECT * FROM customers;
+
+SELECT firstname FROM customers;
+
+SELECT firstname FROM customers WHERE customerid = 1;
+
+UPDATE customers SET firstname = 'Lerato Mabitso' WHERE customerid = 1;
+
+DELETE FROM customers WHERE customerid = 2;
+
+SELECT count(status) FROM orders;
+
+SELECT max(amount) FROM payments;
+
+SELECT * FROM customers ORDER BY country;
+
+SELECT productname FROM products WHERE buyprice BETWEEN 100 AND 600;
+
+SELECT * FROM customers WHERE country = 'Germany' AND city = 'Berlin';
+
+SELECT * FROM customers WHERE city = 'Durban' OR city = 'Cape Town';
+
+SELECT * FROM products WHERE buyprice > 500;
+
+SELECT sum(amount) FROM payments;
+
+SELECT count(status) FROM orders WHERE status = 'Shipped';
+
+SELECT avg(buyprice) FROM products;
+
+SELECT * FROM customers INNER JOIN payments ON payments.customerid = customers.customerid;
+
+SELECT * FROM products WHERE Description LIKE '%Turnable front wheels%';
